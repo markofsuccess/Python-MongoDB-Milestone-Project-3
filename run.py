96eef0b156c8f414cb3e2d21afde8952d6fb25ca
”markofsuccess” <markussjoman1@gmail.com>
2019-12-03 18:22:37 +0000

    added html pages and url app routing

--- run.py
+++ run.py
@@ -1,13 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def index():
+    return render_template("index.html")
 
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+    
+@app.route('/services')
+def services():
+    return render_template("services.html")
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
