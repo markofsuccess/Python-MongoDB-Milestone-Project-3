22e8af85ab150b766bceaa15adc57c44c564abde
markofsuccess <markussjoman1@gmail.com>
2019-12-11 13:50:30 +0000

    added function for insert_text and find function to itemone_review

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -28,11 +28,19 @@ def contact():
 @app.route('/services')
 def services():
     return render_template("services.html")
-    
+
 @app.route('/itemone_review', methods=['POST'])
 def itemone_review():
-    return render_template("itemone_review.html")
-    
+    return render_template("itemone_review.html",
+    specifications=mongo.db.specifications.find())
+
+@app.route('/insert_text', methods=['POST'])
+def insert_text():
+    specifications = mongo.db.specifications
+    specifications.insert_one(request.form.to_dict())
+    return redirect(url_for('itemone_review'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
