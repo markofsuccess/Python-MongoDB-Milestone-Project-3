1473d559e14b052db2e2777a9c2e030380da669b
markofsuccess <markussjoman1@gmail.com>
2019-12-12 14:32:42 +0000

    added GET and if request method

--- run.py
+++ run.py
@@ -33,13 +33,14 @@ def services():
 def itemone_review():
     return render_template("itemone_review.html")
 
-@app.route('/insert_text', methods=['POST'])
+@app.route('/insert_text', methods=['GET','POST'])
 def insert_text():
-    specifications = mongo.db.specifications
-    specifications.insert_one(request.form.to_dict())
+    if request.method == 'POST':
+        specifications = mongo.db.specifications
+        specifications.insert_one(request.form.to_dict())
     return render_template("itemone_review.html",
-    insert_text=mongo.db.specifications.find())
-
+    banana=mongo.db.specifications.find())
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
