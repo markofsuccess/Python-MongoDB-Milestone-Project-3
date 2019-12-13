8ad5913862911bfa97208e2adbedcfd16d309d4c
markofsuccess <markussjoman1@gmail.com>
2019-12-13 10:46:35 +0000

    added return redirect and specify review in itemone_review function

--- run.py
+++ run.py
@@ -4,22 +4,18 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['MONGO_DBNAME'] = os.getenv('MONGO_DBNAME')
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
 @app.route('/about')
 def about():
     return render_template("about.html")
-
 
 @app.route('/contact')
 def contact():
@@ -29,19 +25,20 @@ def contact():
 def services():
     return render_template("services.html")
 
-@app.route('/itemone_review', methods=['POST'])
-def itemone_review():
-    return render_template("itemone_review.html")
 
+@app.route('/itemone_review',methods=['GET','POST'])
+def itemone_review():
+    review=mongo.db.specifications.find()
+    return render_template("itemone_review.html", review=review)
+    
 @app.route('/insert_text', methods=['GET','POST'])
 def insert_text():
     if request.method == 'POST':
         specifications = mongo.db.specifications
         specifications.insert_one(request.form.to_dict())
-    return render_template("itemone_review.html",
-    review=mongo.db.specifications.find())
+    return redirect(url_for('itemone_review',))
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False)
+            debug=True)
